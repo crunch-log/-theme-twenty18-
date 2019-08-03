550afe74d0f4f2dac75039f52b9b3ba14d4336ab
b1thunt3r <ishan@jain.se>
2019-08-03 14:51:37 +0100

    Move source to src

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,12 @@ var sourcemaps = require('gulp-sourcemaps');
 let cleanCSS = require('gulp-clean-css');
 
 gulp.task('less', function(done) {
-    gulp.src('./less/main.less')
+    gulp.src('./src/less/main.less')
         .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(cleanCSS())
         .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./css/'));
+        .pipe(gulp.dest('./src/css/'));
     done();
 });
 
@@ -24,13 +24,13 @@ gulp.task('less', function(done) {
 //});
 
 //gulp.task('watch', gulp.series('browser-sync', function(done) {
-//    gulp.watch("./less/*.less", gulp.series('less'));
+//    gulp.watch("./src/less/*.less", gulp.series('less'));
 //    gulp.watch("*.html").on('change', bs.reload);
-//    gulp.watch("./css/*.css").on('change', bs.reload);
+//    gulp.watch("./stc/css/*.css").on('change', bs.reload);
 //	done();
 //}));
 
 gulp.task('watch',  function(done) {
-    gulp.watch("./less/**/*.less", gulp.series('less'));
+    gulp.watch("./src/less/**/*.less", gulp.series('less'));
     done();
 });
